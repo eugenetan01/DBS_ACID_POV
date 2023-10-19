304dd74d3a8ce201257925fa32c02b62acfa4ca9
Eugene Tan <>
2023-10-19 13:03:24 +0800

    updated transfer.js with new logic - ignore test.js is just for internal testing on functions

--- DBS/functions/test.js
+++ DBS/functions/test.js
@@ -5,10 +5,56 @@ exports = async function(arg){
 const { format } = require('date-fns');
   // Find the name of the MongoDB service you want to use (see "Linked Data Sources" tab)
   const client = context.services.get("mongodb-atlas")
-   const savingsColl = client.db("dbs_savings").collection("savings");
-    
-    const fromAfterAmount = await savingsColl.findOne({account_id: 1449828371});
+   const savingsColl = client.db("dbs_transactions_history").collection("transactions");
+   var resp = {
+     
+   }
+   const res = await savingsColl.insertOne(
+        resp, 
+        );
         
-    console.log(fromAfterAmount)
-    return fromAfterAmount
+    console.log(res.insertedId)
+    
+    resp = {
+    "status": 200,
+    "from": 1449828371,
+    "payerBefore": {
+        "_id": "652f7ef21d23a09d243c25be",
+        "account_id": 1449828371,
+        "name": "Melvin",
+        "email": "keh@gaf.vi",
+        "balance": "96.40000000000000"
+    },
+    "payeeAfter": {
+      "_id": "652f7ef21d23a09d243c25be",
+        "account_id": 1449828371,
+        "name": "Melvin",
+        "email": "keh@gaf.vi",
+        "balance": "100"
+    },
+    "to": 1725503673,
+    "payeeBefore": {
+        "_id": "652f85d12f969dcce83b0148",
+        "account_id": 1725503673,
+        "name": "Lora",
+        "email": "jit@ojovinkuv.et",
+        "balance": "3.60000000000000"
+    },
+    "amount": 1.2
+}
+      
+      await savingsColl.replaceOne(
+        {"_id": res.insertedId}, 
+        resp);
+    //const document = await savingsColl.findOne({account_id: 1449828371});
+  
+
+    //if (document) {
+      // Access the specific field and assign it to the 'result' variable
+      //const result = document.balance; // Replace 'fieldName' with the actual field name
+      //console.log('Result:', result);
+    //} else {
+      //console.log('Document not found');
+    //}
+    //return document
 };
--- DBS/functions/transfer.js
+++ DBS/functions/transfer.js
@@ -1,112 +1,131 @@
 // This function is the endpoint's request handler.
 // This API handles ACID transactions by deducting an amount from balance in origin savings database and add the same amount to the destination current account database in a single transaction.
 
-exports = async function({ query, headers, body}, response) {
-    // Data can be extracted from the request as follows:
-    // This is a binary object that can be accessed as a string using .text()
-    const { format } = require('date-fns');
-    const reqBody = body;
-    const serialized = reqBody.text();
-    // Parse the string into a usable object
-    const parsedBody = JSON.parse(serialized);
-    
-    const client = context.services.get("mongodb-atlas")
-    const savingsColl = client.db("dbs_savings").collection("savings");
-    const currentColl = client.db("dbs_current").collection("current");
-    
-    // add a new transactions collection
-    const txnColl = client.db("dbs_transactions_history").collection("transactions");
-    
-    const session = client.startSession();
-    
-    const from = parsedBody["from"];
-    const to = parsedBody["to"];
-    const amount = parsedBody["amount"];
-    
-    var resp = null;
-    
-    console.log(from);
-    console.log(to);
-    
-    
-    try {
-      session.startTransaction();
-      
-      // find the balance before the deduction in origin savings account
-      const fromBefore = await savingsColl.findOne(
-        {account_id: from});
-        
-      console.log(fromBefore)
-      
-      // update the new balance by deducting amount from balance
-      const fromAfter = await savingsColl.findOneAndUpdate(
-        {account_id: from}, 
-        {$inc: {balance: (-1*amount) }}, 
-        { session, returnNewDocument: true });
-      
-      console.log(fromAfter)
-      
-      // find the new balance after deduction in origin savings account
-      // const fromAfterAmount = await savingsColl.findOne(
-      //  {account_id: from}).balance;
-      
-      // find the new balance before adding amount in destination current account
-      const toBefore = await currentColl.findOne(
-        {account_id: to});
-        
-      // update the new balance by adding amount to balance of destination current account
-      const toAfter = await currentColl.findOneAndUpdate(
-        {account_id: to}, 
-        {$inc: {balance: amount }}, 
-        { session, returnNewDocument: true });
-      
-      // find the new balance after deduction in destination current account
-      //const toAfterAmount = await savingsColl.findOne(
-      //  {account_id: to});
-      
-      // Add a new transaction to the dbs_transactions_history.transactions collection
-      const now = new Date();
-      const iso8601DateTime = now.toISOString();
-      const parsedDate = new Date(iso8601DateTime);
-      
-      console.log(iso8601DateTime);
+exports = async function ({ query, headers, body }, response) {
+  // Data can be extracted from the request as follows:
+  // This is a binary object that can be accessed as a string using .text()
+  const { format } = require("date-fns");
+  const reqBody = body;
+  const serialized = reqBody.text();
+  // Parse the string into a usable object
+  const parsedBody = JSON.parse(serialized);
 
-      await txnColl.insertOne(
-        { 
-          "origin": from,
-          "destination": to,
-          "amount": amount,
-          "updated_time": parsedDate
-        }, 
-        { session });
-      
-      console.log("from: " + from + " to: "+ to + " transferred: " + amount);
-      
-      await session.commitTransaction();
-      console.log("Transaction committed.");
-      
-      
-      resp = {
-        "status": 200,
-        "from": from,
-        "originBefore": fromBefore,
-        "originAfter": fromAfter,
-        "to": to,
-        "destBefore": toBefore,
-        "destAfter": toAfter,
-      }
-      
-    
-    } catch (error) {
-      console.log("An error occurred during the transaction:" + error);
-      await session.abortTransaction();
-      
-      resp = {
-        "status": 501,
-        "message": "An error occurred during the transaction:" + error,
-      }
-    } finally {
-      await session.endSession();
-      return resp
+  const client = context.services.get("mongodb-atlas");
+  const payerColl = client.db("dbs_savings").collection("savings");
+  const payeeColl = client.db("dbs_current").collection("current");
+
+  // add a new transactions collection
+  const txnColl = client
+    .db("dbs_transactions_history")
+    .collection("transactions");
+
+  const session = client.startSession();
+  const txnSession = client.startSession();
+
+  const payer = parsedBody["payer"]; // 1449828371 //;
+  const payee = parsedBody["payee"]; // 1725503673//;
+  const amount = parsedBody["amount"]; // 1.2 //;
+
+  var resp = null;
+
+  try {
+    session.startTransaction();
+
+    // find the balance before the deduction in payee and payer account
+    const payerBefore = await payerColl.findOne({ account_id: payer });
+
+    console.log(payerBefore);
+
+    const payeeBefore = await payeeColl.findOne({ account_id: payee });
+
+    console.log(payeeBefore);
+
+    console.log(payerBefore.balance);
+
+    // If the payer's balance goes below 0, throw an error and exit transaction block
+    const deductionAfter = payerBefore.balance - amount;
+    if (deductionAfter < 0) {
+      throw new TypeError("Balance in payer is less than 0");
     }
-}; 
+
+    // update the new balance in payer by deducting amount from balance
+    await payerColl.updateOne(
+      { account_id: payer },
+      { $inc: { balance: -1 * amount } },
+      { session, returnNewDocument: true }
+    );
+
+    // update the new balance by adding amount to balance of payee account
+    await payeeColl.updateOne(
+      { account_id: payee },
+      { $inc: { balance: amount } },
+      { session, returnNewDocument: true }
+    );
+
+    //const fromAfter = await payerColl.findOneAndUpdate( // change back to findOne to show that we are actually reading the changes from the database
+    //  {account_id: from},
+    //  {$inc: {balance: (-1*amount) }},
+    //  { session, returnNewDocument: true });
+
+    // Add a new transaction to the dbs_transactions_history.transactions collection
+    const now = new Date();
+    const iso8601DateTime = now.toISOString();
+    const parsedDate = new Date(iso8601DateTime);
+
+    resp = {
+      status: 200,
+      from: payer,
+      payerBefore: payerBefore,
+      //"payerAfter": payerAfter,
+      to: payee,
+      payeeBefore: payeeBefore,
+      //"payeeAfter": payeeAfter,
+      amount: amount,
+    };
+
+    const res = await txnColl.insertOne(resp, { session });
+
+    console.log("from: " + payer + " to: " + payee + " transferred: " + amount);
+
+    await session.commitTransaction();
+    console.log("Transfer transaction committed.");
+
+    // Start a new session to find the new amounts after txn has ended and add it to txn history
+    txnSession.startTransaction();
+
+    // find the new balance after deduction in payer account
+    const payerAfter = await payerColl.findOne({ account_id: payer });
+
+    // find the new balance after adding amount in payee account
+    const payeeAfter = await payeeColl.findOne({ account_id: payee });
+
+    resp = {
+      status: 200,
+      from: payer,
+      payerBefore: payerBefore,
+      payerAfter: payerAfter,
+      to: payee,
+      payeeBefore: payeeBefore,
+      payeeAfter: payeeAfter,
+      amount: amount,
+    };
+
+    await txnColl.replaceOne({ _id: res.insertedId }, resp, { txnSession });
+
+    await txnSession.commitTransaction();
+    console.log("Transactions history transaction committed.");
+  } catch (error) {
+    console.log("An error occurred during the transaction:" + error);
+    await session.abortTransaction();
+    await txnSession.abortTransaction();
+
+    resp = {
+      status: 501,
+      message: "An error occurred during the transaction:" + error,
+    };
+  } finally {
+    await session.endSession();
+    await txnSession.endSession();
+    return resp;
+  }
+};
